76dbbe543948e177a917ad41b8304d63db0f0671
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2024-10-17 07:16:45 +0200

    Employee Table View

--- EmployeeManager/employeemanager.client/src/components/employees/EmployeeTable.tsx
+++ EmployeeManager/employeemanager.client/src/components/employees/EmployeeTable.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { EmployeeDTO } from "../../models/employeeDTO";
 import apiConnector from "../../api/apiConnector";
-import { Button, Container } from "semantic-ui-react";
-import EmployeeTableItem from "./EmployeeTableItem";
+import { Button } from "semantic-ui-react";
 import { NavLink, useNavigate } from 'react-router-dom';
 import './EmployeeTable.css';
 
-export default function Table() {
+export default function ListEmployee() {
 
     const [employees, setEmployees] = useState<EmployeeDTO[]>([]);
     const [loading, setLoading] = useState(true);
@@ -28,51 +27,59 @@ export default function Table() {
     }, []);
 
     if (loading) {
-        return <Container className="container-style">
+        return <div className="container-style">
             <div className="empty-state">
                 <img src="/assets/empty-state.JPG" alt="Empty state" className="empty-state-image" />
                 <p>There is nothing here</p>
                 <p>Create a new employee by clicking the</p> <strong>New Employee</strong> <p> button to get started</p>
             </div>
-        </Container>;
+        </div>;
     }
 
     return (
         <>
-            <div className="container-style">
+            <div className="employee-list-container">
                 <div className="header">
                     <h1>Employees</h1>
                     {employees.length === 0 ? <p>No employees</p> : <p>There are {employees.length} employees</p>}
                     <div className="actions">
                         <input type="text" placeholder="Search" className="search-input" />
                         <Button className="filter-button">Filter by</Button>
-                        <Button as={NavLink} to="createEmployee" type="button" content="+ New Employee" positive />
+                        <Button as={NavLink} to="createEmployee" className="new-employee-button" content="+ New Employee" positive />
                     </div>
                 </div>
-            </div>
-            {employees === undefined || employees.length === 0 ? (
-                <Container className="container-style">
+                {employees.length === 0 ? (
                     <div className="empty-state">
                         <img src="/assets/empty-state.JPG" alt="Empty state" className="empty-state-image" />
                         <p>There is nothing here</p>
                         <p>Create a new employee by clicking the</p> <strong>New Employee</strong> <p> button to get started</p>
                     </div>
-                </Container>
-            ) : (
-                    <Container className="container-style">
-                    <table className="ui inverted table">
-                        <thead style={{ textAlign: 'center' }}>
-                        </thead>
-                        <tbody>
-                            {employees.length !== 0 && (
-                                employees.map((employee, index) => (
-                                    <EmployeeTableItem key={index} employee={employee} />
-                                ))
-                            )}
-                        </tbody>
-                    </table>
-                </Container>
-            )}
+                ) : (
+                    <div className="employee-list">
+                        {employees.map((employee, index) => (
+                            <div key={employee.id} className="employee-card">
+                                <span className="employee-id">{index + 1}</span>
+                                <span className="employee-FirstName">{employee.firstName}</span>
+                                <span className="employee-LastName">{employee.lastName}</span>
+                                <span className="employee-PhoneNum">{employee.phoneNum}</span>
+                                <span> <Button as={NavLink} to={'editEmployee/' + employee.userID} color="yellow" type="submit">
+                                    Edit
+                                </Button>
+                                </span>
+                                <span> <Button type="button" negative onClick={async () => {
+                                    await apiConnector.deleteEmployee(employee.userID!);
+                                    window.location.reload();
+                                }}>
+                                    Delete
+                                </Button>
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+
         </>
-    );
+    )
+
 }
